b135691e9674eabcc3c51c2515ade1f34215b1c8
rr0r0 <rori1449@gmail.com>
2024-01-29 11:37:58 +0100

    added mongodb client

---
+++ client.py
@@ -0,0 +1,28 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+
+
+uri = "mongodb+srv://rori1449:6CVCKx0TyqcLptaD@cluster0.zpglndm.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+reviewsdb = client.reviews
+reviewscoll = reviewsdb.reviews
+
+reviewslist = {}
+
+def changeRevFormat():
+    for c, review in enumerate(reviewscoll.find()):
+        reviewdic = {}
+        for v,k in review.items():
+            reviewdic[v]= k
+        reviewslist[c] = reviewdic
+
+    return reviewslist
